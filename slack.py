1163dcb2b494a5d4ea40978ceece666d2166d49d
xaglen <glen.davis@gmail.com>
2023-05-28 10:04:38 -0700

    Put the 10:2 emphasis up top

--- slack.py
+++ slack.py
@@ -64,10 +64,8 @@ def main():
 
             names = random.sample(values, 2)
 
-            slack_message = "We *pray Luke 10:2 at 10:02am every day*, \n>Ask the Lord of the harvest to send out "
-            "workers into His harvest field.\n\nPray for God to call and equip more laborers (both "
-            "globally and specifically here at Stanford)!\n\nIn addition, we pray for two XA members every day"
-            " and today we're praying for"
+            slack_message = "We *pray Luke 10:2 at 10:02am every day*, \n>Ask the Lord of the harvest to send out workers into His harvest field.\n\nPray for God to call and equip more laborers (both "
+            "globally and specifically here at Stanford)!\n\nIn addition, we pray for two XA members every day and today we're praying for"
 
             if len(names[0])==3:
                 slack_message+=  " <@"+names[0][2].strip() + ">"
